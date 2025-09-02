fee21b2e4f90f9b306765234ab930f6fd68a8705
joaoafonso001 <165220475+joaoafonso001@users.noreply.github.com>
2025-09-02 19:57:35 -0300

    Update delete Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,28 +4,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.net.HttpURLConnection;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import main.java.domain.model.Post;
 
-
-// https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations/2.19.2
-// https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core/2.19.2
-// https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind/2.19.2
-
 public class Application {
     ObjectMapper mapper = new ObjectMapper();
 
+   
     public String writeObjectAsStringJson(Object object) throws JsonProcessingException {
         return mapper.writeValueAsString(object);
     }
 
-    public Post fetchPost(Integer id) throws IOException{
+  
+    public Post fetchPost(Integer id) throws IOException {
         URL url = URI.create("https://jsonplaceholder.typicode.com/posts/" + id).toURL();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -37,17 +34,16 @@ public class Application {
         }
     }
 
+  
     public int createPost(Post post) throws IOException {
         URL url = URI.create("https://jsonplaceholder.typicode.com/posts").toURL();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("POST");
         con.setDoOutput(true);
-        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8'");
-        con.setConnectTimeout(5000); 
-        con.setReadTimeout(5000);
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
 
         try (OutputStream os = con.getOutputStream();
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"))) {
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"))) {
             writer.write(mapper.writeValueAsString(post));
             writer.flush();
         }
@@ -56,17 +52,54 @@ public class Application {
         return con.getResponseCode();
     }
 
-    public int updatePost(int postId) {
 
+    public int updatePost(int postId, Post post) throws IOException {
+        URL url = URI.create("https://jsonplaceholder.typicode.com/posts/" + postId).toURL();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("PUT");
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+        try (OutputStream os = con.getOutputStream();
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"))) {
+            writer.write(mapper.writeValueAsString(post));
+            writer.flush();
+        }
+
+        con.connect();
+        return con.getResponseCode();
+    }
+
+
+    public int deletePost(int postId) throws IOException {
+        URL url = URI.create("https://jsonplaceholder.typicode.com/posts/" + postId).toURL();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("DELETE");
+
+        con.connect();
+        return con.getResponseCode();
     }
 
     public static void main(String[] args) throws IOException {
         Application app = new Application();
+
+       
         Post post = app.fetchPost(10);
+        System.out.println("GET Post:");
         System.out.println(app.writeObjectAsStringJson(post));
 
+       
         Post novoPost = new Post(101, 12345, "Teste title", "teste body");
-        int statusCode = app.createPost(novoPost);
-        System.out.println(statusCode);
+        int statusCodeCreate = app.createPost(novoPost);
+        System.out.println("Status code (Create): " + statusCodeCreate);
+
+        
+        novoPost.setTitle("Título atualizado");
+        int statusCodeUpdate = app.updatePost(101, novoPost);
+        System.out.println("Status code (Update): " + statusCodeUpdate);
+
+        
+        int statusCodeDelete = app.deletePost(101);
+        System.out.println("Status code (Delete): " + statusCodeDelete);
     }
 }
